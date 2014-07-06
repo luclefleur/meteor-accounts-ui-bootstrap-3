897bc2a8df27fd103b621b68da350ebdb919b8cf
gdiab <me@georgediab.com>
2014-07-06 09:40:20 -0700

    Update to accounts_ui.js to fix requestPermissions
    requestPermissions was being set twice

--- accounts_ui.js
+++ accounts_ui.js
@@ -66,18 +66,6 @@ Accounts.ui.config = function(options) {
 			}
 		}
 	}
-    // deal with `requestPermissions`
-    if (options.requestPermissions) {
-        _.each(options.requestPermissions, function(scope, service) {
-            if (Accounts.ui._options.requestPermissions[service]) {
-                throw new Error("Accounts.ui.config: Can't set `requestPermissions` more than once for " + service);
-            } else if (!(scope instanceof Array)) {
-                throw new Error("Accounts.ui.config: Value for `requestPermissions` must be an array");
-            } else {
-                Accounts.ui._options.requestPermissions[service] = scope;
-            }
-        });
-    }
 
     if (options.extraSignupFields) {
         if (typeof options.extraSignupFields !== 'object' || !options.extraSignupFields instanceof Array) {
